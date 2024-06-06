f25d36061cd3b23f42b0eb2fbd8359002cd3618c
Jekon4ik <ghfswhrh@gmail.com>
2024-06-06 19:19:59 +0200

    task2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// task1    
 function findMiddleCharacter() {
     let userInput = prompt("Enter a word:");
     if (userInput === null) {
@@ -23,5 +24,32 @@ function findMiddleCharacter() {
     alert(result);
     console.log(result);
 }
+//task2
+import { users } from './users_task2.js';
+function getUniqueSkills(users) {
+    const skillsSet = new Set();
 
-findMiddleCharacter();
+    users.forEach(user => {
+        user.skills.forEach(skill => {
+            skillsSet.add(skill);
+        });
+    });
+    
+    return Array.from(skillsSet);
+}
+function getTotalUsers(users) {
+    const filteredUsers = users.filter(user => {
+        return user.friends.length === 2 && user.age < 30;
+    });
+    const totalBalance = filteredUsers.reduce((total, user) => {
+        return total + user.balance;
+    }, 0);
+    return totalBalance;
+}
+//task1
+//findMiddleCharacter();
+
+
+//task2
+//console.log(getUniqueSkills(users));
+//console.log(getTotalUsers(users));
